2d9b9a5b4ce69ba220cc191c44c86b8b6f94ace1
Gabriel Nelle <nelle@raceresult.com>
2017-11-13 16:14:02 +0100

    add more / improve profiling

--- profile.go
+++ profile.go
@@ -1,16 +1,43 @@
 package dbg
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
 	"runtime/pprof"
+	"strconv"
+	"sync/atomic"
 )
 
 var (
-	fileCPU string
+	fileCPU            string
+	executableFileName string
+	fileNumber         uint64
 )
 
-func CPUProfileStart(fileName string) error {
+type ProfileType string
+
+const (
+	ProfileWebChart ProfileType = "web"
+	ProfileWebList              = "weblist=.*"
+	ProfileList                 = "list=.*"
+)
+
+func SetExecutableName(executable string) {
+	executableFileName = executable
+}
+
+func FlameGraph(seconds int, pprofURL ...string) error {
+	url := "http://localhost:6060/debug/pprof/profile"
+	if len(pprofURL) != 0 {
+		url = pprofURL[0]
+	}
+	return urlFlameGraph(seconds, "torch.svg", url)
+}
+
+func CPUProfileStart() error {
+	fileName := getFileName("profile_%d.cpu")
 	f, err := os.Create(fileName)
 	if err != nil {
 		return err
@@ -23,22 +50,128 @@ func CPUProfileStart(fileName string) error {
 	return err
 }
 
-// CPUProfileOpenBrowser creates a go-torch cpu flamegraph and opens it in the browser
+// CPUProfileFlameGraph creates a go-torch cpu flamegraph and opens it in the browser
 // Needs gotorch to be installed with flamegraph scripts in PATH
 // TODO: make open cmd plattform independent
-func CPUProfileOpenBrowser() error {
+func CPUProfileFlameGraph() error {
 	pprof.StopCPUProfile()
-	cmd := exec.Command("go-torch", fileCPU)
+	return fileFlameGraph(fileCPU, executableFileName)
+}
+
+func CPUProfile(profile ProfileType) error {
+	pprof.StopCPUProfile()
+	return createReport(getProfileString(profile), fileCPU, executableFileName)
+}
+
+func GoroutineProfile(profile ProfileType) error {
+	fileName := getFileName("profile_%d.goroutine")
+	if err := writeProfile("goroutine", fileName); err != nil {
+		return err
+	}
+	return createReport(getProfileString(profile), fileName, executableFileName)
+}
+
+func HeapProfile(profile ProfileType) error {
+	fileName := getFileName("profile_%d.heap")
+	if err := writeProfile("heap", fileName); err != nil {
+		return err
+	}
+	return createReport(getProfileString(profile), fileName, executableFileName)
+}
+
+func ThreadcreateProfile(profile ProfileType) error {
+	fileName := getFileName("profile_%d.thread")
+	if err := writeProfile("threadcreate", fileName); err != nil {
+		return err
+	}
+	return createReport(getProfileString(profile), fileName, executableFileName)
+}
+
+func BlockProfileStart() {
+	runtime.SetBlockProfileRate(1)
+}
+
+func BlockProfile(profile ProfileType) error {
+	fileName := getFileName("profile_%d.block")
+	if err := writeProfile("block", fileName); err != nil {
+		return err
+	}
+	return createReport(getProfileString(profile), fileName, executableFileName)
+}
+
+func MutexProfileStart() {
+	runtime.SetMutexProfileFraction(1)
+}
+
+func MutexProfile(profile ProfileType) error {
+	fileName := getFileName("profile_%d.mutex")
+	if err := writeProfile("mutex", fileName); err != nil {
+		return err
+	}
+	return createReport(getProfileString(profile), fileName, executableFileName)
+}
+
+func writeProfile(profileName, fileName string) error {
+	f, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+
+	runtime.GC()
+	if err := pprof.Lookup(profileName).WriteTo(f, 0); err != nil {
+		return fmt.Errorf("could not write %s profile: %s", profileName, err.Error())
+	}
+	f.Close()
+	return nil
+}
+
+func urlFlameGraph(seconds int, fileName, url string) error {
+	svgFile := fmt.Sprintf("%s.svg", fileName)
+	cmd := exec.Command("go-torch", "--seconds", strconv.Itoa(seconds), "-f", svgFile, url)
+	return openFlameGraph(cmd, svgFile)
+}
+
+func fileFlameGraph(fileName, executable string) error {
+	svgFile := fmt.Sprintf("%s.svg", fileName)
+	cmd := exec.Command("go-torch", "-f", svgFile, executable, fileName)
+	return openFlameGraph(cmd, svgFile)
+}
+
+func openFlameGraph(cmd *exec.Cmd, svgFile string) error {
 	if b, err := cmd.CombinedOutput(); err != nil {
 		Debug(string(b))
 		Debug("error running go-torch:", err)
 		return err
 	}
-	cmd = exec.Command("open", "torch.svg")
+	cmd = exec.Command("open", svgFile)
 	if b, err := cmd.CombinedOutput(); err != nil {
 		Debug(string(b))
-		Debug("error opening torch.svg:", err)
+		Debug("error opening", svgFile, ":", err)
 		return err
 	}
 	return nil
+}
+
+func createReport(report, fileName, executable string) error {
+	paramReport := fmt.Sprintf("-%s", report)
+	cmd := exec.Command("go", "tool", "pprof", paramReport, executable, fileName)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		Debug("error running go-torch:", err)
+		return err
+	}
+	return nil
+}
+
+func getFileName(filePattern string) string {
+	atomic.AddUint64(&fileNumber, 1)
+	return fmt.Sprintf(filePattern, atomic.LoadUint64(&fileNumber))
+}
+
+func getProfileString(profile ProfileType) string {
+	switch profile {
+	default:
+		return string(profile)
+	}
 }
--- profile_test.go
+++ profile_test.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 )
 
-func TestCPUProfile(t *testing.T) {
-	if err := CPUProfileStart("test.cpu"); err != nil {
+func TestCPUProfileFlameGraph(t *testing.T) {
+	if err := CPUProfileStart(); err != nil {
 		fmt.Println("failed to start profiling")
 		t.Error(err)
 		return
@@ -20,7 +20,7 @@ func TestCPUProfile(t *testing.T) {
 	}
 	wg.Wait()
 
-	if err := CPUProfileOpenBrowser(); err != nil {
+	if err := CPUProfileFlameGraph(); err != nil {
 		t.Error(err)
 	}
 }
@@ -31,4 +31,127 @@ func count(wg *sync.WaitGroup) {
 		n++
 	}
 	wg.Done()
+}
+
+func TestCPUProfile(t *testing.T) {
+	var (
+		mu    sync.Mutex
+		items = make(map[int]struct{})
+		wg    = &sync.WaitGroup{}
+	)
+
+	SetExecutableName("server")
+	if err := CPUProfileStart(); err != nil {
+		fmt.Println("failed to start profiling")
+		t.Error(err)
+		return
+	}
+
+	wg.Add(20 * 1000)
+	for i := 0; i < 20*1000; i++ {
+		go func(i int, wg *sync.WaitGroup) {
+			mu.Lock()
+			defer mu.Unlock()
+
+			items[i] = struct{}{}
+			wg.Done()
+		}(i, wg)
+	}
+	wg.Wait()
+
+	if err := CPUProfile(ProfileList); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestMutexProfile(t *testing.T) {
+	var (
+		mu    sync.Mutex
+		items = make(map[int]struct{})
+		wg    = &sync.WaitGroup{}
+	)
+
+	SetExecutableName("server")
+	MutexProfileStart()
+
+	wg.Add(20 * 1000)
+	for i := 0; i < 20*1000; i++ {
+		go func(i int, wg *sync.WaitGroup) {
+			mu.Lock()
+			defer mu.Unlock()
+
+			items[i] = struct{}{}
+			wg.Done()
+		}(i, wg)
+	}
+	wg.Wait()
+
+	MutexProfile(ProfileList)
+}
+
+func TestBlockProfile(t *testing.T) {
+	var (
+		mu    sync.Mutex
+		items = make(map[int]struct{})
+		wg    = &sync.WaitGroup{}
+	)
+
+	SetExecutableName("server")
+	BlockProfileStart()
+
+	wg.Add(20 * 1000)
+	for i := 0; i < 20*1000; i++ {
+		go func(i int, wg *sync.WaitGroup) {
+			mu.Lock()
+			defer mu.Unlock()
+
+			items[i] = struct{}{}
+			wg.Done()
+		}(i, wg)
+	}
+	wg.Wait()
+
+	BlockProfile(ProfileList)
+}
+
+func TestHeapProfile(t *testing.T) {
+	var (
+		mu    sync.Mutex
+		items = make(map[int]struct{})
+		wg    = &sync.WaitGroup{}
+	)
+
+	SetExecutableName("server")
+
+	wg.Add(20 * 1000)
+	for i := 0; i < 20*1000; i++ {
+		go func(i int, wg *sync.WaitGroup) {
+			mu.Lock()
+			defer mu.Unlock()
+
+			items[i] = struct{}{}
+			wg.Done()
+		}(i, wg)
+	}
+	wg.Wait()
+
+	HeapProfile(ProfileList)
+}
+
+func TestThreadcreateProfile(t *testing.T) {
+	var (
+		wg = &sync.WaitGroup{}
+	)
+
+	SetExecutableName("server")
+
+	wg.Add(20 * 1000)
+	for i := 0; i < 20*1000; i++ {
+		go func(i int, wg *sync.WaitGroup) {
+			wg.Done()
+		}(i, wg)
+	}
+	wg.Wait()
+
+	ThreadcreateProfile(ProfileList)
 }
